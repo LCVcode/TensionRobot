78bc11a2ead1c67c44b4ef2f64a3960cd48340fd
LCVcode <lcvcode@gmail.com>
2021-10-07 21:54:00 -0600

    Main event loop and first draw

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from src.render import RenderWindow
 
 if __name__ == "__main__":
     win: RenderWindow = RenderWindow(480, 330)
+    win.main_loop(0.03)
--- src/render.py
+++ src/render.py
@@ -10,3 +10,20 @@ class RenderWindow:
 
     def __init__(self, width: int, height: int):
         self._DISPLAY = pg.display.set_mode((width, height))
+
+    def main_loop(self, time_step: float = 0):
+        running: bool = True
+
+        while running:
+            t_start: float = time.time()
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+            self.draw_all()
+            while time.time() - t_start < time_step:
+                pass
+
+    def draw_all(self) -> None:
+        self._DISPLAY.fill((0, 0, 0))
+        pg.draw.circle(self._DISPLAY, (120, 30, 0), (0, 0), 50)
+        pg.display.flip()
